c4ddeb57bce7be113294770256698c7b0df4d946
Ajay Raj S <49017115+ajayr4j@users.noreply.github.com>
2026-05-22 07:53:11 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        len_nums = len(nums)
+        h = len_nums
+        while l <= h:
+            m = (l + h)//2
+            print(l,m,h, len_nums)
+            if m > len_nums - 1:
+                break
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                h = m - 1
+            else:
+                return m
+        return -1
+
